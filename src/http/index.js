d6ae2b6fdd825ff08649f06928c2d5c296fa46ba
majkl zumberi <majklzumberi00@gmail.com>
2021-02-24 22:57:19 +0100

    fix: :wrench: fix axios config

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-export default (() =>
-  axios.create({
-    baseURL: process.env.BASEURL,
-    params: {
-      appid: process.env.APIKEY,
-      lang: process.env.LANG,
-      units: process.env.UNITS
-    }
-  }))();
+export default axios.create({
+  baseURL: process.env.VUE_APP_BASEURL,
+  params: {
+    appid: process.env.VUE_APP_APIKEY,
+    lang: process.env.VUE_APP_LANG,
+    units: process.env.VUE_APP_UNITS
+  }
+});
