424184a47d3aea9d6eeb005bbe1af964cc49a0d3
majkl zumberi <majklzumberi00@gmail.com>
2021-02-24 21:36:51 +0100

    feat(mockup): :clown_face: add mockup for left section

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,105 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-      <!-- <router-view></router-view> -->
-    </v-main>
+      <v-container fluid>
+        <v-layout row>
+          <v-flex xs12 md3>
+            <div class="side-container">
+              <v-row class="pa-8" align="center" justify="space-around">
+                <v-btn depressed small class="font-weight-regular" @click.stop="drawer = !drawer">
+                  Cerca località</v-btn>
+                <v-btn depressed fab small>
+                  <v-icon>gps_fixed</v-icon>
+                </v-btn>
+              </v-row>
+              <div class="current-weather-bg-wrapper">
+              <div id="weather">
+                <v-row align="center" justify="center">
+                    <v-img  src="http://openweathermap.org/img/wn/10d@2x.png"></v-img>
+                  </v-row>
+              </div>
+            </div>
+            <div class="d-flex flex-column
+             align-center justify-space-around align-content-space-between" >
+              <h2
+              class="text-h1 white--text font-weight-medium">15<span
+               class="display-1">&#8451;</span></h2>
+              <p class="blue-grey--text text--lighten-2 font-weight-medium display-1">Shower</p>
+              <div>
+                <span class="body-1 grey--text font-weight-regular ">Today</span>
+                <span class="body-1 grey--text font-weight-regular px-2">•</span>
+                <span class="body-1 grey--text font-weight-regular ">Fri 5 jun</span>
+                <div>
+                  <p
+                  class="body-1 grey--text font-weight-regular text-center">
+                  <span><v-icon color="grey">location_on</v-icon></span> italy</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </v-flex>
+          <v-flex xs12 md9>
+            <div class="side-container2">
+              <p>weila</p>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
-    //
+    drawer: false,
   }),
 };
+
 </script>
+<style>
+  div .d-flex{
+    height: 39vh;
+  }
+  .side-container {
+    padding: 0;
+    margin: 0;
+    height: 101.7vh;
+    background-color: #1e2139;
+  }
+
+  .side-container2 {
+    height: 100vh;
+    background-color: #100e1c;
+  }
+
+  .current-weather-bg-wrapper {
+    position: relative;
+    height: 45vh;
+    overflow: hidden;
+}
+
+.current-weather-bg-wrapper #weather {
+    padding: 50px;
+    position: relative;
+    z-index: 2;
+}
+/* You could use :after - it doesn't really matter */
+.current-weather-bg-wrapper:before {
+    content: ' ';
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    opacity: 0.1;
+    background-image: url('/img/Cloud-background.png');
+    background-repeat: no-repeat;
+    background-position: 50% 0;
+    background-size: cover;
+}
+</style>
