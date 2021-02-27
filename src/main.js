724fbac7bb755eac7fcdecdd94037ee3709f397e
majkl zumberi <majklzumberi00@gmail.com>
2021-02-27 20:23:19 +0100

    feat(main): :sparkles: add vuetify-dialog into main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import Vue from "vue";
+// need instance of vuetify, for example
+import VuetifyDialog from "vuetify-dialog";
+import "vuetify-dialog/dist/vuetify-dialog.css";
 import App from "./App.vue";
 import http from "./http";
 import vuetify from "./plugins/vuetify";
@@ -10,6 +13,11 @@ import store from "./store";
 Vue.config.productionTip = false;
 Vue.prototype.$axios = http;
 
+Vue.use(VuetifyDialog, {
+  context: {
+    vuetify
+  }
+});
 new Vue({
   router,
   store,
