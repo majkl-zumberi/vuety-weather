995323688738ba33d461fd3e754068da41280bc0
majkl zumberi <majklzumberi00@gmail.com>
2021-02-27 20:30:25 +0100

    feat(eventBus): :sparkles: handle global events through eventBus in app.vue

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@
 <script>
 import AsideContainer from "@/components/AsideContainer.vue";
 import MainContainer from "@/components/MainContainer.vue";
+import eventBus from "@/utils/eventBus";
 
 export default {
   name: "App",
@@ -29,8 +30,53 @@ export default {
     current: {},
     daily: {}
   }),
+  methods: {
+    async getCityWeather({ city, lat, lon }) {
+      this.city = city;
+
+      const { data: weatherData } = await this.$axios.get(`data/2.5/onecall?lat=${lat}&lon=${lon}`);
+      this.current = { ...weatherData.current };
+      this.daily = weatherData.daily;
+    },
+    async searchCity(city) {
+      try {
+        const { data } = await this.$axios(
+          `${process.env.VUE_APP_POSITIONSTACK_BASEURL}?access_key=${process.env.VUE_APP_POSITIONSTACK_APIKEY}&query=${city}`
+        );
+
+        if (Array.isArray(data.data) && data.data.length > 1) {
+          /**
+           * es: inserisco budapest (ungheria? usa? ecc.)
+           * ho trovato più risultati per quella città
+           */
+
+          this.$dialog.notify.error(`più occorrenze trovate per ${city}, sii più specifico`, {
+            position: "top-right",
+            timeout: 5000
+          });
+        } else {
+          const location = {
+            city: `${data.data[0].name}, ${data.data[0].country}`,
+            lat: data.data[0].latitude,
+            lon: data.data[0].longitude
+          };
+          this.getCityWeather({ ...location });
+        }
+      } catch (error) {
+        this.$dialog.notify.error(`nessuna occorrenza per ${city}`, {
+          position: "top-right",
+          timeout: 3000
+        });
+      }
+    },
+    async getCurrentLocation() {
+      const { data } = await this.$axios.get(`${process.env.VUE_APP_LOCATION}`);
+      const location = { city: data.city, lat: data.lat, lon: data.lon };
+      this.getCityWeather({ ...location });
+    }
+  },
   async created() {
-    const { data } = await this.$axios.get("http://ip-api.com/json");
+    const { data } = await this.$axios.get(`${process.env.VUE_APP_LOCATION}`);
     const { city, country, lat, lon } = data;
     this.city = `${city}, ${country}`;
     this.lat = lat;
@@ -41,15 +87,21 @@ export default {
     );
     this.current = { ...weatherData.current };
     this.daily = weatherData.daily;
+
+    eventBus.$on("city-changed", this.getCityWeather);
+    eventBus.$on("search-city", this.searchCity);
+    eventBus.$on("current-location", this.getCurrentLocation);
+  },
+  beforeDestroy() {
+    eventBus.$off("city-changed", this.getCityWeather);
+    eventBus.$off("search-city", this.searchCity);
+    eventBus.$off("current-location", this.getCurrentLocation);
   }
 };
 </script>
 <style>
 body {
   background-color: #100e1c;
-}
-div .d-flex {
-  height: 39vh;
 }
 
 .bg-background {
